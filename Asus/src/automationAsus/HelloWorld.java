7e7e601deed24ee360ef40e7784f2d51df08448e
Me <Me@DESKTOP-6DOKBJB>
2024-02-24 23:34:33 +0530

    new sum1 commited

--- Asus/src/automationAsus/HelloWorld.java
+++ Asus/src/automationAsus/HelloWorld.java
@@ -4,8 +4,13 @@ public class HelloWorld
 {
 public static void main(String[] args)
 {
-	int a=50, b=20;
+	int a=10, b=20;
 	int sum=a+b;
 	System.out.println(sum);
+	
+	int c=10, d=20;
+	int sum1=c+d;
+	System.out.println(sum1);
+	
 }
 }
