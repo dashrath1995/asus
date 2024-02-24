68497adbe8249108f1ebb1a1ff995ee235e4bdac
Me <Me@DESKTOP-6DOKBJB>
2024-02-24 21:27:17 +0530

    updated

--- Asus/src/automationAsus/HelloWorld.java
+++ Asus/src/automationAsus/HelloWorld.java
@@ -1,7 +1,11 @@
 package automationAsus;
 
-public class HelloWorld {
-public static void main(String[] args) {
+public class HelloWorld 
+{
+public static void main(String[] args)
+{
 	System.out.println("hello world");
+	System.out.println("my name is Dashrath Pradhan");
+	System.out.println("I am 27 years old");
 }
 }
