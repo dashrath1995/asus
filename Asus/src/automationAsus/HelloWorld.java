0f279f5a6a4b91918e16c9e1c13174b822f31c2b
Me <Me@DESKTOP-6DOKBJB>
2024-02-24 23:40:26 +0530

    new sum1

--- Asus/src/automationAsus/HelloWorld.java
+++ Asus/src/automationAsus/HelloWorld.java
@@ -6,6 +6,10 @@ public static void main(String[] args)
 {
 	int a=10, b=20;
 	int sum=a+b;
-	System.out.println(sum);	
+	System.out.println(sum);
+	
+	int c=10, d=20;
+	int sum1=c+d;
+	System.out.println(sum1);	
 }
 }
