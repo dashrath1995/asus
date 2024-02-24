6fff8a649f2928d88eb2b8a49bb087418bff248a
Me <Me@DESKTOP-6DOKBJB>
2024-02-24 23:23:14 +0530

    new code added

--- Asus/src/automationAsus/HelloWorld.java
+++ Asus/src/automationAsus/HelloWorld.java
@@ -4,13 +4,9 @@ public class HelloWorld
 {
 public static void main(String[] args)
 {
-<<<<<<< HEAD
 	int a=10, b=20;
 	int sum=a+b;
 	System.out.println(sum);
 	System.out.println("successfull");
-=======
-	System.out.println("hello world");
->>>>>>> refs/heads/master
 }
 }
