4d822471a0e7d0481b32abf7ec033c440da417c2
Me <Me@DESKTOP-6DOKBJB>
2024-02-24 23:26:43 +0530

    sajhkv

--- Asus/src/automationAsus/HelloWorld.java
+++ Asus/src/automationAsus/HelloWorld.java
@@ -7,8 +7,5 @@ public static void main(String[] args)
 	int a=10, b=20;
 	int sum=a+b;
 	System.out.println(sum);
-	System.out.println("successfull");
-	System.out.println("fail");
-	System.out.println("again fail");
 }
 }
