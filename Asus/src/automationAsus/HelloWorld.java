0282976d24e16f059958b85badd97265ee874513
Me <Me@DESKTOP-6DOKBJB>
2024-02-24 22:51:39 +0530

    master branch commited

--- Asus/src/automationAsus/HelloWorld.java
+++ Asus/src/automationAsus/HelloWorld.java
@@ -5,8 +5,5 @@ public class HelloWorld
 public static void main(String[] args)
 {
 	System.out.println("hello world");
-
-	System.out.println("my name is Dashrath Pradhan");
-	System.out.println("I am 27 years old");
 }
 }
